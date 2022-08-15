a63b1b948f83881886384d85e07f246b0387291b
Leszek Łabuda <leszek.labuda@o2.pl>
2022-08-15 17:49:03 +0200

    Changing links in the footer

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,8 +1,8 @@
 <div class="container">
     <ul class="repository-list">
         <li><a class="link" href="https://github.com/leszeklabuda" target="_blank">GitHub</a></li>
-        <li><a class="link" href="https://codepen.io/leszeklabuda/pens/public" target="_blank">CodePen</a></li>
-        <li><a class="link" href="https://jsfiddle.net/user/leszeklabuda/fiddles" target="_blank">JSFiddle</a></li>
+        <li><a class="link" href="https://codepen.io/leszeklabuda" target="_blank">CodePen</a></li>
+        <li><a class="link" href="https://jsfiddle.net/user/leszeklabuda/fiddles/" target="_blank">JSFiddle</a></li>
     </ul>
     <em class="copyright">&copy; 2022 Leszek Łabuda</em>
 </div>
